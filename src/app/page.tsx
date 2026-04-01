5540c9a26a1086ed44256dc77d062a0c1d6e4b03
Hanan <Hanan.bayazeed56@gmail.com>
2026-04-01 23:44:24 +0300

    feat(testimonials) build the testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/organisms/Navbar";
+import Testimonials from "@/components/organisms/Testimonials";
 export default function page() {
   return (
     <div>
       <Navbar />
+      <Testimonials />
     </div>
   );
 }

3a8647652c2a59cbe378b59fd7bc0b3b060e1a57
Hanan <Hanan.bayazeed56@gmail.com>
2026-04-01 23:51:35 +0300

    feat(gallery/build gallery section)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/organisms/Navbar";
 import Features from "@/components/organisms/Features";
+import Gallery from "@/components/organisms/Gallery";
 
 export default function page() {
   return (
     <div>
       <Navbar />
       <Features />
+      <Gallery />
     </div>
   );
 }

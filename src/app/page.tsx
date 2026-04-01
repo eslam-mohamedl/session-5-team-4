78be71b261c10db5118e51bde5cc754bc6d41409
seifelsayed <seif.elsayed.official@gmail.com>
2026-04-01 23:07:33 +0200

    hotfix(about): Updated font style, color, size and family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Navbar from "@/components/organisms/Navbar";
 import Testimonials from "@/components/organisms/Testimonials";
 import Team from "@/components/organisms/Team";
 import Features from "@/components/organisms/Features";
+import About from "@/components/organisms/About";
 
 export default function page() {
   return (
     <div>
       <Navbar />
+      <About />
       <Testimonials />
       <Team />
       <Features />

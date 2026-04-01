1fdc874ab3a02733ffab974c38d224a5d08f6c78
m3hmoud7 <mahmoudkhaled183@gmail.com>
2026-04-01 22:45:57 +0200

    team component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/organisms/Navbar";
+import Team from "@/components/organisms/Team";
 export default function page() {
   return (
     <div>
       <Navbar />
+      <Team />
     </div>
   );
 }

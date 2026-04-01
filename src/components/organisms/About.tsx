a9e9f7d065272a69cc4f60ef981842d01f3171b6
seifelsayed <seif.elsayed.official@gmail.com>
2026-04-01 22:40:58 +0200

    feat(about): Add About page

---
+++ src/components/organisms/About.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="bg-cyan-600 text-black font-[Poppins] p-5 flex justify-center items-center">
+      About
+    </div>
+  );
+}

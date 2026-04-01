a9cb0de7a9242bb4aa4dac910498c585c5b649c5
Yasmin Hillis <.com>
2026-04-01 23:27:02 +0200

    style(features): change features bg style

--- src/components/organisms/Features.tsx
+++ src/components/organisms/Features.tsx
@@ -1,6 +1,6 @@
 export default function Features() {
   return (
-    <div className="bg-red-300 text-white p-5 flex justify-center items-center">
+    <div className="bg-violet-400 text-white p-5 flex justify-center items-center">
       Features
     </div>
   );
